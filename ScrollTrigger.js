3c725b814728274b224f679f335bf3c57377ec6f
Erik Terwan <erik@erikterwan.com>
2016-07-01 12:24:06 +0200

    Fixed the callback scrollTop

--- ScrollTrigger.js
+++ ScrollTrigger.js
@@ -142,7 +142,7 @@
 			for (var n = 0; n < attached.length; n++) {
 				var callback = attached[n];
 
-				callback.call(_this, windowHeight, _this.scrollElement.scrollTop);
+				callback.call(_this, windowHeight, _this.bindElement.scrollTop);
 			}
 			
 			// and loop again
