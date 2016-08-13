9cd76cada3742767dcea708fbcb6be46f16e8e5e
erikterwan <erik@erikterwan.com>
2016-08-13 16:02:51 +0200

    Fixed minor bug

--- ScrollTrigger.js
+++ ScrollTrigger.js
@@ -143,6 +143,8 @@ var ScrollTrigger = (function(){
 				return function(){
 					// set the default options
 					var options = _this.defaultOptions;
+					// parse the options given in the data-scroll attribute, if any
+					var optionString = _this.element.getAttribute('data-scroll');
 
 					if (options) {
 						if (options.toggle && options.toggle.visible) {
@@ -187,8 +189,7 @@ var ScrollTrigger = (function(){
 						_this.once = optionString.indexOf("once") > -1;
 					}
 
-					// parse the options given in the data-scroll attribute, if any
-					var optionString = _this.element.getAttribute('data-scroll');
+					// parse callbacks
 					_this.showCallback = _this.element.getAttribute('data-scroll-showCallback');
 					_this.hideCallback = _this.element.getAttribute('data-scroll-hideCallback');
 					
@@ -299,8 +300,6 @@ var ScrollTrigger = (function(){
 				// the data-scroll attribute and turn it from a NodeList
 				// into a plain old array
 				triggers = [].slice.call(_this.bindElement.querySelectorAll("[data-scroll]"));
-
-				console.log(defaultOptions);
 
 				// map all the triggers to Trigger objects, and initialize them
 				// so the options get parsed
