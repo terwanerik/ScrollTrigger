91ca02c29cba17de87c0ddb014127ac4d94f88d3
erikterwan <erik@erikterwan.com>
2017-07-11 11:20:07 +0200

    Fixed demo with lazy loading images Added build

--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
 import ScrollTrigger from './ScrollTrigger'
 
-const scroll = new ScrollTrigger()
+const scroll = new ScrollTrigger({
+	scroll: {
+		callback: (position, direction) => {
+			const stats = document.querySelector('div.stats')
+
+			stats.innerHTML = 'pos: ' + JSON.stringify(position)
+			stats.innerHTML += ' [' + direction + ']'
+		}
+	}
+})
+
 scroll.add("div.block")
+
+const lazyTriggers = scroll.query('[data-lazy]')
+
+lazyTriggers.forEach((trigger) => {
+	trigger.toggle.callback.in = loadImage
+})
+
+function loadImage(trigger) {
+	const url = this.getAttribute('data-lazy')
+	const img = document.createElement('img')
+	img.src = url
+
+	this.appendChild(img)
+	this.removeAttribute('data-lazy')
+	
+	trigger.toggle.callback.in = null
+}
